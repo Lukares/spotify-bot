f2ff8d20565291c09ace949cf75a4e6c754895ad
Lukares <luke_reddick@yahoo.com>
2018-11-13 00:48:00 -0600

    Bot Joins Channel

--- spotify-bot.js
+++ spotify-bot.js
@@ -1,4 +1,4 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
@@ -8,32 +8,21 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
+var bot = new Discord.Client();
+bot.login(auth.token);
+
+bot.on('ready', evt => {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+
+bot.on('message', async message => {
+    if (!message.guild) return;
+    const text = message.toString();
+    const voice_id = (message.member.voiceChannelID);
+    const voice_channel = message.guild.channels.get(voice_id);
+    if (text.substring(0, 7) === 'spotify' && voice_channel) {
+      const connection = await voice_channel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
 });
