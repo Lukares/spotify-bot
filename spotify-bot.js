f214b14cde61a4e046b2201a18e218fa48ea1ace
Lukares <luke_reddick@yahoo.com>
2018-11-14 19:43:23 -0600

    WIP

--- spotify-bot.js
+++ spotify-bot.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
+const ytdl = require('ytdl-core');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,21 +10,31 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client();
+const bot = new Discord.Client();
 bot.login(auth.token);
 
 bot.on('ready', evt => {
-    logger.info('Connected');
+    logger.info('Initialized.');
 });
 
 bot.on('message', async message => {
-    if (!message.guild) return;
-    const text = message.toString();
-    const voice_id = (message.member.voiceChannelID);
-    const voice_channel = message.guild.channels.get(voice_id);
-    if (text.substring(0, 7) === 'spotify' && voice_channel) {
-      const connection = await voice_channel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  console.log(message);
+  const voice_id = (message.member.voiceChannelID);
+  const voice_channel = message.guild.channels.get(voice_id);
+  const streamOptions = {seek: 0, volume: 1};
+  const stream = ytdl('https://www.youtube.com/watch?v=FyASdjZE0R0', {filter: 'audioonly'});
+
+  if (message.content.substring(0, 7) === 'spotify' && voice_channel) {
+    voice_channel.join().then(connection => {
+        const dispatcher = connection.playStream(stream, streamOptions);
+        dispatcher.on('finish', () => {
+            console.log('Finished playing!');
+            dispatcher.destroy();
+        });
+    });
+  } else {
+      message.reply('You need to be in a voice channel first!');
+  }
 });
